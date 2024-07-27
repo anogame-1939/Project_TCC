c533f421febcbd4bebe8ef35b3dbb12c48c353b8
AnoGame <nan_mo_nasi@yahoo.co.jp>
2024-07-28 05:18:30 +0900

    とりあえずジョイスティックでプレイヤーを動かせるように修正

---
+++ Assets/AnoGame/Controller/OnScreenSlider.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+using UnityEngine.UI;
+using JoystickPack;
+
+public class OnScreenSlider : OnScreenControl
+{
+    public VariableJoystick variableJoystick;
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = new Vector2(variableJoystick.Horizontal, variableJoystick.Vertical);
+        Debug.Log(direction);
+        SendValueToControl(direction);
+    }
+
+    private void OnValueChanged(Vector2 direction)
+    {
+        SendValueToControl(direction);
+    }
+
+    [InputControl(layout = "Stick")]
+    [SerializeField]
+    private string _controlPath = "<Gamepad>/leftStick";
+
+    protected override string controlPathInternal
+    {
+        get => _controlPath;
+        set => _controlPath = value;
+    }
+}
--- Assets/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
+++ Assets/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FixedJoystick : Joystick
+namespace JoystickPack
 {
+    public class FixedJoystick : Joystick
+    {
 
+    }
 }
