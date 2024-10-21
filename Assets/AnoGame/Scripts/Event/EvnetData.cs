8c5fb80b3763b1220063ac08d400d64b18f98d55
AnoGame <nan_mo_nasi@yahoo.co.jp>
2024-10-21 12:44:58 +0900

    WIP EventDataの定義を決定

--- Assets/AnoGame/Scripts/Event/EvnetData.cs
+++ Assets/AnoGame/Scripts/Event/EvnetData.cs
@@ -1,18 +1,23 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace AnoGame.Event
 {
     [Serializable]
-    public class EvnetData : ScriptableObject
+    public class EvnetData
     {
-        public int EvnentID;
-        public int EvnentName;
-        public int EvnentType;
-        
-        
-        
+        public int EvnentID = 0;
+        public string EvnentName = "default";
+        public EventType EvnentType = EventType.Normal;
+        [SerializeField]
+        public List<GameObject> _spwanObjects;
     }
+
+    public enum EventType
+    {
+        Normal,
+
+    }
+
 }
--- Assets/AnoGame/Scripts/Event/EvnetDataList.cs
+++ Assets/AnoGame/Scripts/Event/EvnetDataList.cs
@@ -8,12 +8,5 @@ namespace AnoGame.Event
     {
         [SerializeField]
         List<EvnetData> _eventDataList = new List<EvnetData>();
-
-
-        [SerializeField] SerializedDictionary<int, EvnetData> m_PrimitiveDic;
-        [SerializeField] SerializedDictionary<string> m_StringDic;
-        [SerializeField] SerializedDictionaryC<Vector3, string> m_ClassDid;
-
-        [SerializeField] SerializedDictionaryC<int, EvnetData> m_ClassDid2;
     }
 }
