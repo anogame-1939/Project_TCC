c0cc081014e72ed9d5bfd3577a6540db215bd20d
AnoGame <nan_mo_nasi@yahoo.co.jp>
2025-01-28 12:50:56 +0900

    UI 修正

--- Assets/AnoGame/Scripts/Application/Inventory/InventorySlotComponent.cs
+++ Assets/AnoGame/Scripts/Application/Inventory/InventorySlotComponent.cs
@@ -13,6 +13,8 @@ namespace AnoGame.Application.Inventory
 
         public void OnPointerEnter(PointerEventData eventData)
         {
+            Debug.Log("Pointer Enter");
+            Debug.Log(_inventorySlot.CurrentItem.ItemName);
             InventoryItemDetail.Instance.SetText(_inventorySlot.CurrentItem.ItemName, _inventorySlot.CurrentItem.Description);
         }
 
