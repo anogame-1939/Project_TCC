ea3d70577fa29532f05f72f979469d31c5edc246
tanaka123 <krsmgr@gmail.com>
2025-06-08 17:50:29 +0900

    インベントリをゲームパッドで操作できるように修正

--- Assets/AnoGame/Scripts/Application/Inventory/InventorySlotComponent.cs
+++ Assets/AnoGame/Scripts/Application/Inventory/InventorySlotComponent.cs
@@ -3,7 +3,7 @@ using UnityEngine.EventSystems;
 
 namespace AnoGame.Application.Inventory
 {
-    public class InventorySlotComponent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    public class InventorySlotComponent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler
     {
         InventorySlot _inventorySlot;
         void Start()
@@ -20,6 +20,12 @@ namespace AnoGame.Application.Inventory
 
         public void OnPointerExit(PointerEventData eventData)
         {
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            Debug.Log($"選択された: {gameObject.name}");
+            InventoryItemDetail.Instance.SetText(_inventorySlot.LocalizedName, _inventorySlot.LocalizedDescription);
         }
     }
 }
