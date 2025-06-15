9963c8de1821520d2d46bb599dc7def86c1b86c0
AnoGame <nan_mo_nasi@yahoo.co.jp>
2025-06-15 14:20:00 +0900

    WIP

--- Assets/AnoGame/Scripts/Application/GameOver/GameOverUI.cs
+++ Assets/AnoGame/Scripts/Application/GameOver/GameOverUI.cs
@@ -1,24 +1,28 @@
 using UnityEngine;
 using AnoGame.Application.Event;
+using AnoGame.Application.UI;
 
 namespace AnoGame.Application.GameOver
 {
     public class GameOverUI : MonoBehaviour
     {
         [SerializeField]
-        private GameObject gameOverPanel;
+        private UISection section;
 
         private void Awake()
         {
             GameOverManager.Instance.GameOver += ShowGameOverPanel;
             HideGameOverPanel();
 
-            DontDestroyOnLoad(this);
+            // DontDestroyOnLoad(this);
+            section.panel.SetActive(false);
         }
 
         private void ShowGameOverPanel()
         {
-            gameOverPanel.SetActive(true);
+            section.panel.SetActive(true);
+            section.selectables[0].Select();
+
             // カーソルを表示する
             Cursor.visible = true;
             // 必要に応じてロックを解除する場合は以下も追加
@@ -27,7 +31,7 @@ namespace AnoGame.Application.GameOver
 
         private void HideGameOverPanel()
         {
-            gameOverPanel.SetActive(false);
+            section.panel.SetActive(false);
             // ゲーム中はカーソルを非表示にする
             Cursor.visible = false;
             // 必要に応じてロック状態に戻す場合は以下も追加
