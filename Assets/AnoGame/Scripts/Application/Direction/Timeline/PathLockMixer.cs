24cb8e36d6998a0a99071de3f2e4b01138449196
AnoGame <nan_mo_nasi@yahoo.co.jp>
2025-09-12 00:31:11 +0900

    [bugfix]エディタ開きなおすとタイムライン上のPathLockが壊れる問題を修正

--- Assets/AnoGame/Scripts/Application/Direction/Timeline/PathLockMixer.cs
+++ Assets/AnoGame/Scripts/Application/Direction/Timeline/PathLockMixer.cs
@@ -1,16 +1,6 @@
-using UnityEngine;
 using UnityEngine.Playables;
-using UnityEngine.Timeline;
 using AnoGame.Application.Player.Control; // EventLockControl の名前空間
 
-[TrackColor(0.3f, 0.8f, 1f)]
-[TrackClipType(typeof(PathLockPlayableAsset))]
-[TrackBindingType(typeof(EventLockControl))] // バインド先：EventLockControl
-public class PathLockTrack : TrackAsset
-{
-    public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
-        => ScriptPlayable<PathLockMixer>.Create(graph, inputCount);
-}
 
 public class PathLockMixer : PlayableBehaviour
 {
