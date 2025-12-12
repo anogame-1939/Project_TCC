a48dbe8c9c1ef7a3eed77411f7afdf8243395ba3
AnoGame <nan_mo_nasi@yahoo.co.jp>
2025-12-12 13:09:25 +0900

    WIP
    Revert "WIP"
    
    This reverts commit 40131e74fb8ad6538b9bf7381b25790b05f04566.
    
    WIP スクリプト追加
    
    WIP
    
    Revert "WIP"
    
    This reverts commit a8fa165d56e2f3bafb5feceaf3c650472b7f8159.
    
    Reapply "WIP"
    
    This reverts commit ce7396712d6077b3225f25d42242fae2a29506a3.
    
    Reapply "WIP"
    
    This reverts commit db34a944258fc6f4131439ed452090f0b7d48cfd.
    
    WIP イベント修正
    
    WIP イベント追加中

--- Assets/AnoGame/Scripts/Application/Event/TriggerExitEventInvoker.cs
+++ Assets/AnoGame/Scripts/Application/Event/TriggerExitEventInvoker.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(Collider))]
-public class TriggerEventInvoker : MonoBehaviour
+public class TriggerExitEventInvoker : MonoBehaviour
 {
-    public UnityEvent onTriggerEnter;
+    public UnityEvent onTriggerExit;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         // 何らかの条件で判定する
         if (other.CompareTag("Player"))
         {
             // UnityEvent を呼び出す
-            onTriggerEnter?.Invoke();
+            onTriggerExit?.Invoke();
         }
     }
 }
