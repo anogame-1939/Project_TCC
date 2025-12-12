1309b24864b6329ca39aaf1b833fe5af5ca8283a
AnoGame <nan_mo_nasi@yahoo.co.jp>
2025-12-12 14:33:51 +0900

    [bugfix] タイムラインが二重起動される問題を修正

--- Assets/AnoGame/Scripts/Application/Event/TimelineController.cs
+++ Assets/AnoGame/Scripts/Application/Event/TimelineController.cs
@@ -9,10 +9,24 @@ namespace AnoGame.Application.Event
     {
         [SerializeField] private PlayableDirector _playableDirector;
 
+        // Enqueueの多重実行防止用フラグ
+        private bool _isEnqueued = false;
+
         public void Enqueue()
         {
-            Debug.Log("TimelineController: Enqueueing timeline.", this);
+            if (_isEnqueued)
+            {
+                Debug.LogWarning($"TimelineController: Enqueue skipped (already enqueued) for {_playableDirector.name}", this);
+                return;
+            }
+            _isEnqueued = true;
+            Debug.Log($"TimelineController: Enqueueing timeline...{_playableDirector.name}", this);
             TimelineQueueManager.Instance.Enqueue(new TimelineTask(_playableDirector));
+        }
+        // Enqueue完了時に呼び出すことで再度Enqueue可能にする
+        public void ResetEnqueueFlag()
+        {
+            _isEnqueued = false;
         }
 
         private void OnValidate()
