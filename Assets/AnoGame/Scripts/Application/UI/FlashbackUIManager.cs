f2f0e52e92f8754eae526f5a57e30250ec360f2d
AnoGame <nan_mo_nasi@yahoo.co.jp>
2025-12-22 00:51:23 +0900

    WIP 回想の表示を調整

--- Assets/AnoGame/Scripts/Application/UI/FlashbackUIManager.cs
+++ Assets/AnoGame/Scripts/Application/UI/FlashbackUIManager.cs
@@ -7,10 +7,13 @@ namespace AnoGame.Application.UI
     public class FlashbackUIManager : SingletonMonoBehaviour<FlashbackUIManager>
     {
         [SerializeField]
+        private Image _bgImage;
+        [SerializeField]
         private Image _image;
 
         private void Start()
         {
+            _bgImage.enabled = false;
             _image.enabled = false;
         }
 
@@ -21,11 +24,13 @@ namespace AnoGame.Application.UI
 
         public void Show()
         {
+            _bgImage.enabled = true;
             _image.enabled = true;
         }
 
         public void Hide()
         {
+            _bgImage.enabled = false;
             _image.enabled = false;
         }
     }
