a5327f304ad7939fff7c87c619833557fce74b19
AnoGame <nan_mo_nasi@yahoo.co.jp>
2025-12-22 18:37:34 +0900

    WIP 調べる1回のみ実行なオプション追加、他

--- Assets/AnoGame/Scripts/Application/Player/Interaction/InspectZone.cs
+++ Assets/AnoGame/Scripts/Application/Player/Interaction/InspectZone.cs
@@ -8,19 +8,28 @@ namespace AnoGame.Application.Player.Interaction
     {
         [SerializeField] private string prompt = "調べる（長押し）";
         [SerializeField] private int priority = 500;
+        [SerializeField] private bool _once = false;
 
         public UnityEvent OnInspected; // Timeline再生/ログ追加など
 
+        private bool _isExecuted = false;
+
         public override bool TryBuildOptions(Transform actor, List<InteractionOption> buffer)
         {
+            if (_once && _isExecuted) return false;
             if (!InDistance(actor) || !InAngle(actor) || !HasLoS(actor)) return false;
 
-            buffer.Add(new InteractionOption{
+            buffer.Add(new InteractionOption
+            {
                 Kind = InteractionKind.Inspect,
                 Prompt = prompt,
                 Priority = priority,
                 RequiresHold = true,
-                Execute = () => OnInspected?.Invoke()
+                Execute = () =>
+                {
+                    OnInspected?.Invoke();
+                    _isExecuted = true;
+                }
             });
             return true;
         }
