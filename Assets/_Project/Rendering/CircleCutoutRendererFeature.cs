bb9fc054c55822026203c2c3ff31236210e9f5b0
AnoGame <21275458+anogame-1939@users.noreply.github.com>
2026-03-07 02:08:46 +0900

    Revert "WIP"
    This reverts commit 284be5f17b3443c673ea3b3d6eb974367df9e224.

--- Assets/_Project/Rendering/CircleCutoutRendererFeature.cs
+++ Assets/_Project/Rendering/CircleCutoutRendererFeature.cs
@@ -30,10 +30,7 @@ namespace AnoGame.Application.Rendering
             };
             _cutoutPass = new CircleCutoutRenderPass
             {
-                // AfterRenderingTransparents: 全Opaque/Transparent描画完了後に合成を実行
-                // BeforeRenderingPostProcessing ではSpeedTree等の一部シェーダが
-                // まだ描画されていない場合があるため、より確実なタイミングを使用
-                renderPassEvent = RenderPassEvent.AfterRenderingTransparents
+                renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing
             };
         }
 
